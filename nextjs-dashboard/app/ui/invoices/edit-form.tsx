4d728745686b923e7827a5ed881794d6583040e3
DANIEL <dandrichest@gmail.com>
2026-03-23 14:37:49 +0100

    Finally completed

--- nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -9,7 +9,10 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
+import { customers } from '@/app/lib/placeholder-data';
+ 
 export default function EditInvoiceForm({
   invoice,
   customers,
@@ -17,7 +20,12 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  return (
+  const initialState: State = { message: null, errors: {} };
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+ 
+  return <form action={formAction}>{/* ... */}</form>;
+}
     <form>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
@@ -119,5 +127,4 @@ export default function EditInvoiceForm({
         <Button type="submit">Edit Invoice</Button>
       </div>
     </form>
-  );
-}
+  
